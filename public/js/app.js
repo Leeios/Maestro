ea426b5752ee4396de0bf18ed1f08cb20343dbb7
Hugo DEZIER <hdezier@e1r10p8.42.fr>
2014-06-21 20:38:22 +0200

    test

--- public/js/app.js
+++ public/js/app.js
@@ -29,57 +29,8 @@ var sphereShape, sphereBody, groundBody;
 /*MAIN*/
 function main() {
 
-init();
-initCannon();
-animate();
-
-}
-
-function initCannon(){
-	// Setup our world
-	world = new CANNON.World();
-	world.quatNormalizeSkip = 0;
-	world.quatNormalizeFast = false;
-
-	var solver = new CANNON.GSSolver();
-
-	world.defaultContactMaterial.contactEquationStiffness = 1e9;
-	world.defaultContactMaterial.contactEquationRegularizationTime = 4;
-
-	solver.iterations = 7;
-	solver.tolerance = 0.1;
-	var split = true;
-	if(split)
-		world.solver = new CANNON.SplitSolver(solver);
-	else
-		world.solver = solver;
-
-	world.gravity.set(0,0,0);
-	world.broadphase = new CANNON.NaiveBroadphase();
-
-	// Create a slippery material (friction coefficient = 0.0)
-	physicsMaterial = new CANNON.Material("slipperyMaterial");
-	var physicsContactMaterial = new CANNON.ContactMaterial(physicsMaterial,
-															physicsMaterial,
-															0.0, // friction coefficient
-															0.3  // restitution
-															);
-	// We must add the contact materials to the world
-	world.addContactMaterial(physicsContactMaterial);
-
-	// Create a sphere
-	var mass = 5, radius = 1.3;
-	sphereShape = new CANNON.Sphere(radius);
-	sphereBody = new CANNON.RigidBody(mass,sphereShape,physicsMaterial);
-	sphereBody.position.set(0,5,0);
-	sphereBody.linearDamping = 0.9;
-	world.add(sphereBody);
-
-	// Create a plane
-	var groundShape = new CANNON.Plane();
-	var groundBody = new CANNON.RigidBody(0,groundShape,physicsMaterial);
-	groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
-	world.add(groundBody);
+	init();
+	animate();
 }
 
 function init() {
@@ -90,25 +41,16 @@ function init() {
 
 	scene = new THREE.Scene();
 
-	data = generateHeight( worldWidth, worldDepth );
-
-	var geometry = new THREE.PlaneGeometry( 7500, 7500, worldWidth - 1, worldDepth - 1 );
+	geometry = new THREE.PlaneGeometry( 2000, 2000, 2000, 50 );
 	geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
-
-	for ( var i = 0, l = geometry.vertices.length; i < l; i ++ ) {
-
-		geometry.vertices[ i ].y = data[ i ] * 10;
-
-	}
-
-	texture = new THREE.Texture( generateTexture( data, worldWidth, worldDepth ), new THREE.UVMapping(), THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping );
-	texture.needsUpdate = true;
-
-	mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { map: texture } ) );
+	material = new THREE.MeshLambertMaterial( { color: 0xdddddd } );
+	mesh = new THREE.Mesh( geometry, material );
+	mesh.castShadow = true;
+	mesh.receiveShadow = true;
 	scene.add( mesh );
 
-	geometry = new THREE.BoxGeometry( 400, 400, 400 );
-	material = new THREE.MeshBasicMaterial( {color: 0x45f8d3} );
+	geometry = new THREE.SphereGeometry( 50 );
+	material = new THREE.MeshBasicMaterial( {color: 0x0000ff} );
 	player = new THREE.Mesh( geometry, material );
 	camera.position.y = 500;
 	camera.position.z = 1500;
@@ -139,94 +81,6 @@ function init() {
 
 }
 
-function generateHeight( width, height ) {
-
-	var size = width * height, data = new Uint8Array( size ),
-	perlin = new ImprovedNoise(), quality = 1, z = Math.random() * 100;
-
-	for ( var j = 0; j < 4; j ++ ) {
-
-		for ( var i = 0; i < size; i ++ ) {
-
-			var x = i % width, y = ~~ ( i / width );
-			data[ i ] += Math.abs( perlin.noise( x / quality, y / quality, z ) * quality * 1.75 );
-
-		}
-
-		quality *= 5;
-
-	}
-
-	return data;
-
-}
-
-function generateTexture( data, width, height ) {
-
-	var canvas, canvasScaled, context, image, imageData,
-	level, diff, vector3, sun, shade;
-
-	vector3 = new THREE.Vector3( 0, 0, 0 );
-
-	sun = new THREE.Vector3( 1, 1, 1 );
-	sun.normalize();
-
-	canvas = document.createElement( 'canvas' );
-	canvas.width = width;
-	canvas.height = height;
-
-	context = canvas.getContext( '2d' );
-	context.fillStyle = '#000';
-	context.fillRect( 0, 0, width, height );
-
-	image = context.getImageData( 0, 0, canvas.width, canvas.height );
-	imageData = image.data;
-
-	for ( var i = 0, j = 0, l = imageData.length; i < l; i += 4, j ++ ) {
-
-		vector3.x = data[ j - 2 ] - data[ j + 2 ];
-		vector3.y = 2;
-		vector3.z = data[ j - width * 2 ] - data[ j + width * 2 ];
-		vector3.normalize();
-
-		shade = vector3.dot( sun );
-
-		imageData[ i ] = ( 96 + shade * 128 ) * ( 0.5 + data[ j ] * 0.007 );
-		imageData[ i + 1 ] = ( 32 + shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
-		imageData[ i + 2 ] = ( shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
-	}
-
-	context.putImageData( image, 0, 0 );
-
-	// Scaled 4x
-
-	canvasScaled = document.createElement( 'canvas' );
-	canvasScaled.width = width * 4;
-	canvasScaled.height = height * 4;
-
-	context = canvasScaled.getContext( '2d' );
-	context.scale( 4, 4 );
-	context.drawImage( canvas, 0, 0 );
-
-	image = context.getImageData( 0, 0, canvasScaled.width, canvasScaled.height );
-	imageData = image.data;
-
-	for ( var i = 0, l = imageData.length; i < l; i += 4 ) {
-
-		var v = ~~ ( Math.random() * 5 );
-
-		imageData[ i ] += v;
-		imageData[ i + 1 ] += v;
-		imageData[ i + 2 ] += v;
-
-	}
-
-	context.putImageData( image, 0, 0 );
-
-	return canvasScaled;
-
-}
-
 /*Final functions*/
 
 function onWindowResize() {
@@ -239,6 +93,7 @@ function onWindowResize() {
 
 var dt = 1/60;
 function animate() {
+
 	requestAnimationFrame( animate );
 
 	render();
@@ -252,9 +107,3 @@ function render() {
 	renderer.render( scene, camera );
 
 }
-
-window.addEventListener("click",function(e){
-	if(controls.enabled==true){
-		;
-	}
-});
